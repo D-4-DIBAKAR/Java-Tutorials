a1ccbbf30bae7afa57a2923982fc5466e85ea225
DIBAKAR PARIDA <dibakarparida586@gmail.com>
2024-10-03 12:07:39 +0530

    Variables

---
+++ Basics/Datatypes.java
@@ -0,0 +1,53 @@
+package Basics;
+
+//Data Types
+public class Datatypes {
+     public static void main(String[] args) {
+          // Whole Number
+          /*
+           * byte
+           * short
+           * int
+           * long
+           */
+          // Decimal Number
+          /*
+           * float
+           * double
+           */
+          // Boolean
+          /*
+           * true
+           * false
+           */
+          // Character & String
+          /*
+           * char
+           * String
+           */
+
+          // Sizes of Various Data types
+          /*
+           * Byte : -128 to 127
+           * Short : -32768 to 32767
+           * Integer : -2147483648 to 2147483647
+           * Long : -9223372036854775808 to 9223372036854775807
+           */
+          System.out.println("Byte : " + Byte.MIN_VALUE + " to " + Byte.MAX_VALUE);
+
+          System.out.println("Short : " + Short.MIN_VALUE + " to " + Short.MAX_VALUE);
+
+          System.out.println("Integer : " + Integer.MIN_VALUE + " to " + Integer.MAX_VALUE);
+
+          System.out.println("Long : " + Long.MIN_VALUE + " to " + Long.MAX_VALUE);
+
+          /*
+           * Float : 1.4E-45 to 3.4028235E38
+           * Double : 4.9E-324 to 1.7976931348623157E308
+           */
+          System.out.println("Float : " + Float.MIN_VALUE + " to " + Float.MAX_VALUE);
+
+          System.out.println("Double : " + Double.MIN_VALUE + " to " + Double.MAX_VALUE);
+
+     }
+}
---
+++ Basics/Variable.java
@@ -0,0 +1,20 @@
+package Basics;
+
+//Variables
+public class Variable {
+     public static void main(String[] args) {
+          // case sensitive
+          // can be letters,digits,signs or underscore
+          // Must begin with a letter, Dollar Sign($) or Underscore(_)
+          // Can not use Java Keywords as variable name
+          int $age = 23;// Ex-1
+          System.out.println($age);
+          int _age = 24;// Ex-1
+          System.out.println(_age);
+          // int 1age = 23;//Ex- Not allowed
+          // System.out.println(1age);
+          int a = 10;
+          double b = 2.5;
+          System.out.println(a + " " + b);
+     }
+}
