1bce6422a3bf417b9e7832370d01b791e837e76f
DIBAKAR PARIDA <dibakarparida586@gmail.com>
2024-10-10 13:00:22 +0530

    Arrays

--- Arrays/SumOfArray.java
+++ Arrays/SumOfArray.java
@@ -1,16 +1,18 @@
 package Arrays;
 
-public class ReverseArray {
+public class SumOfArray {
      public static void main(String[] args) {
-          // Reverse An Array
+          // Sum of Array Element
           int[] arr = new int[5];
           arr[0] = 2;
           arr[1] = 12;
           arr[2] = 22;
           arr[3] = -32;
           arr[4] = 42;
-          for (int i = arr.length - 1; i >= 0; i--) {
-               System.out.println(arr[i]);
+          int sum = 0;
+          for (int i = 0; i < arr.length; i++) {
+               sum += arr[i];
           }
+          System.out.println(sum);
      }
 }
