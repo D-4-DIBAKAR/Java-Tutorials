885a4355b8021bf9aff7289dded95533b388a0cf
DIBAKAR PARIDA <dibakarparida586@gmail.com>
2024-10-10 12:56:43 +0530

    Arrays

--- Arrays/ReverseArray.java
+++ Arrays/ReverseArray.java
@@ -1,6 +1,6 @@
 package Arrays;
 
-public class FindMaxElement {
+public class ReverseArray {
      public static void main(String[] args) {
           // Find max Element in An Array
           int[] arr = new int[5];
@@ -9,13 +9,8 @@ public class FindMaxElement {
           arr[2] = 22;
           arr[3] = -32;
           arr[4] = 42;
-
-          int res = Integer.MIN_VALUE;
-          for (int i = 0; i < arr.length; i++) {
-               if (arr[i] > res) {
-                    res = arr[i];
-               }
+          for (int i = arr.length - 1; i >= 0; i--) {
+               System.out.println(arr[i]);
           }
-          System.out.println(res);
      }
 }
